b02dc79e63d4e6c61efa164253415c3cb0d96294
Rahul Jain <jainrahu@arcesium.com>
2019-09-21 20:24:43 +0530

    changes in slot

--- archetype-dao/src/main/java/com/arcesium/archetypewar/dao/BookingDaoImpl.java
+++ archetype-dao/src/main/java/com/arcesium/archetypewar/dao/BookingDaoImpl.java
@@ -1,23 +1,26 @@
 package com.arcesium.archetypewar.dao;
 
 import com.arcesium.archetypewar.domain.Booking;
-import com.arcesium.archetypewar.domain.utils.SlotUtil;
+import com.arcesium.archetypewar.domain.Slot;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 
+import javax.inject.Inject;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class BookingDaoImpl extends SqlSessionDaoSupport implements BookingDao {
+
+    @Inject
+    private Slot slot;
+
     @Override
     public Booking bookSlot(Booking booking) {
         Map<String, Object> param = new HashMap<String, Object>();
         param.put("bookingId", booking.getBookingId());
         param.put("userId", booking.getUser().getUserId());
-        List<Integer> slots = SlotUtil.getSlotIds(booking.getSlot());
-        param.put("startTime", slots.get(0));
-        param.put("endTime", slots.get(1));
-        param.put("bookingDate", booking.getSlot().getStartTime().toDate());
+        param.put("startTime", slot.getTime(booking.getStartTime(), booking.getDate()));
+        param.put("endTime", slot.getTime(booking.getEndTime(), booking.getDate()));
+        param.put("bookingDate", booking.getDate());
         param.put("players", booking.getPlayers());
         getSqlSession().insert("insertBookingData", param);
         return booking;
--- archetype-domain/src/main/java/com/arcesium/archetypewar/domain/Slot.java
+++ archetype-domain/src/main/java/com/arcesium/archetypewar/domain/Slot.java
@@ -1,26 +1,43 @@
 package com.arcesium.archetypewar.domain;
 
-import org.joda.time.DateTime;
-
-import java.sql.Date;
+import java.util.Date;
+import java.sql.Time;
+import java.util.Calendar;
 
 public class Slot {
-    private DateTime startTime;
-    private DateTime endTime;
+    private Time startTime;
+    private Time endTime;
+    private Date date;
 
-    public DateTime getStartTime() {
+    public Time getStartTime() {
         return startTime;
     }
 
-    public void setStartTime(DateTime startTime) {
+    public void setStartTime(Time startTime) {
         this.startTime = startTime;
     }
 
-    public DateTime getEndTime() {
+    public Time getEndTime() {
         return endTime;
     }
 
-    public void setEndTime(DateTime endTime) {
+    public void setEndTime(Time endTime) {
         this.endTime = endTime;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getTime(int timeSlot, Date date){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, timeSlot/2);
+        cal.set(Calendar.MINUTE, (timeSlot%2)*30);
+        return cal.getTime();
     }
 }
