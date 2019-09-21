f6a88f726df2bfd2298e10e57948795dd812d544
vermat <vermatanya1994@gmail.com>
2019-09-21 16:03:13 +0530

    slotUtil

---
+++ archetype-common/src/main/java/com/arcesium/archetypewar/commons/utils/SlotUtil.java
@@ -0,0 +1,18 @@
+package com.arcesium.archetypewar.commons.utils;
+
+import com.arcesium.archetypewar.domain.Slot;
+import org.joda.time.Duration;
+
+import java.util.List;
+
+
+public class SlotUtil {
+
+    public static List<Integer> getSlotIds(Slot slot) {
+
+        Duration duration = new Duration(slot.getStartTime(), slot.getEndTime());
+        final long standardHours = duration.getStandardHours();
+
+        return null;
+    }
+}
---
+++ archetype-dao/src/main/java/com/arcesium/archetypewar/dao/SearchDao.java
@@ -0,0 +1,4 @@
+package com.arcesium.archetypewar.dao;
+
+public interface SearchDao {
+}
